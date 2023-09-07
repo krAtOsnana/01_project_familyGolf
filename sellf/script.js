e41f1305f53418a73b3fb3ee1d432e3f9e2bfb30
kushagra_kumar <96875614+krAtOsnana@users.noreply.github.com>
2023-09-07 09:48:38 +0000

    page2ScrollTrigger

--- sellf/script.js
+++ sellf/script.js
@@ -2,11 +2,27 @@ var crsr= document.querySelector("#cursor")
 var blurr= document.querySelector("#blurr")
 
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left=dets.x + "px";
+    crsr.style.left=dets.x+15 + "px";
     crsr.style.top=dets.y + "px";
     blurr.style.left=dets.x -250 + "px";
     blurr.style.top=dets.y -250 + "px";
 })
+
+var h4all=document.querySelectorAll("#navi h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=2
+        crsr.style.border="1px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1
+        crsr.style.border="0px solid #95C11E"
+        crsr.style.backgroundColor="#95C11E"
+    })
+})
+
+
 gsap.to("#navi",{
   
    backgroundColor :"#000",
@@ -35,3 +51,40 @@ gsap.to("#main",{
 }
 )
  
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:4,
+    stagger:0.3,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 58%",
+        scrub:5
+
+    }
+})
+gsap.to("#colon1",{
+    y:45,
+    x:50,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.to("#colon2",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
